0e383b60a5ffee25323782cfa04a8869b4020661
shalotts <francyfox@yandex.ru>
2024-02-07 11:10:09 +0600

    fix without vavite

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,22 @@ import type { UserConfig } from 'vite';
 import { defaultViteF3vConfig } from 'f3v';
 import { defu } from 'defu';
 import UnoCSS from 'unocss/vite';
-import { vavite } from 'vavite';
 import vue from '@vitejs/plugin-vue';
 import ssr from 'vike/plugin';
 import path from 'node:path';
-import url from 'node:url';
 
-const root = path.dirname(url.fileURLToPath(import.meta.url));
+const { buildSteps, ...config } = defaultViteF3vConfig;
+
 
 const myConfig = {
+  server: {
+    port: 3000,
+  },
   plugins: [
-    vavite({
-      handlerEntry: '/handler.ts',
-      serveClientAssetsInDev: true,
-    }),
+    // vavite({
+    //   handlerEntry: '/handler.ts',
+    //   serveClientAssetsInDev: true,
+    // }),
     ssr(),
     vue({
       include: [/\.vue$/, /\.md$/],
@@ -35,9 +37,9 @@ const myConfig = {
   resolve: {
     dedupe: ['vue'],
     alias: {
-      "#root": root,
+      '#': path.resolve('.'),
     },
   },
 } satisfies UserConfig
 
-export default defineConfig(defu(defaultViteF3vConfig, myConfig));
+export default defineConfig(defu(config, myConfig));
