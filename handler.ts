461ab0152bd150069a48a08467e3ff8efc221218
shalotts <francyfox@yandex.ru>
2024-02-12 09:18:43 +0600

    save before restore

--- handler.ts
+++ handler.ts
@@ -1,15 +1,26 @@
-import awsLambdaFastify from '@fastify/aws-lambda'
+import awsLambdaFastify from '@fastify/aws-lambda';
 import { AppModule, HttpModule } from 'f3v';
+import { IncomingMessage, ServerResponse } from 'node:http';
 import config from './sha.config.ts';
-export const proxy = async () => {
+export const App = async () => {
   const http = new HttpModule(config.fastifyInstanceOptions);
   const FastifyInstance = await http.createServer();
   const $sha = new AppModule(FastifyInstance, config);
   await $sha.create();
+  await $sha.app.ready();
 
-  console.log('CREATED');
-
-  return awsLambdaFastify($sha.app)
+  return $sha.app;
 };
 
-export default { proxy };
+export const proxy = async () => {
+  const app = await App();
+  return awsLambdaFastify(app);
+}
+
+export const handler = async (request: IncomingMessage, reply: ServerResponse) => {
+  const app = await App();
+
+  app.server.emit('request', request, reply);
+}
+
+export default handler;
